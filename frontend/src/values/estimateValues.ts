4682764fd4d877ed51f3bb295e9c45776e552264
D-Stella <80246452+D-Stella@users.noreply.github.com>
2021-04-05 09:58:32 +0900

    officer name 수정

--- frontend/src/values/estimateValues.ts
+++ frontend/src/values/estimateValues.ts
@@ -3,6 +3,6 @@ export const estimateProject = [
   { name: 'KORLOY PRODUCT', category: ['I/S', 'T/H'] },
 ];
 
-export const officerList = ['Mr.Kim', 'Ms.Erin', 'Ms.Eva', 'Ms.Jane', 'Ms.Susan'];
+export const officerList = ['Mr. Kim', 'Ms. Erin', 'Mrs. Eva', 'Ms. Jane', 'Ms. Susan'];
 export const attnList = ['Seong-ho Park', 'Hak-min Kim'];
 export const attnList2 = ['Eva', 'Erin', 'Jane', 'Susan'];
